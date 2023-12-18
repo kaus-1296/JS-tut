fb9f1dc9bf18bb3db11b884e5736d203a45e22f0
kaus-1296 <105623408+kaus-1296@users.noreply.github.com>
2023-12-18 10:13:14 +0000

    comparison done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,5 +18,12 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 3
 let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber) 
+
+
+// Operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,1 @@
-console.log("kaustav");
+console.log("kaustav Majumdar");
