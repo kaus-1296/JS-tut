34ae3dc4dbe066c0990bec1caf900efc7b6564ad
kaus-1296 <105623408+kaus-1296@users.noreply.github.com>
2023-12-18 07:32:42 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJS, not browser
+
+let name = "kaustav"
+let age = 27
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null)  // object
